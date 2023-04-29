4f8de823a1bd341b0f09b2cf047b2715290ea044
treeep <bogdan.grigorenko.jb@gmail.com>
2023-04-29 21:49:01 +0000

    feat(dev/#2-login) user login endpoint

--- demo/src/main/java/com/cbshop/demo/user/UserService.java
+++ demo/src/main/java/com/cbshop/demo/user/UserService.java
@@ -1,7 +1,24 @@
 package com.cbshop.demo.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.Base64;
+
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserService {
+
+    private final UserRepository userRepository;
+    public void loginUser(User user){
+        User userByEmail = userRepository.findByEmail(user.getEmail())
+                .orElseThrow(RuntimeException::new);
+
+        if(!userByEmail.getPassword()
+                .equals(Arrays.toString(Base64.getDecoder().decode(user.getPassword())))){
+            throw new RuntimeException();
+        }
+    }
 }
