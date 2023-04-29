41f11da61fed45e477dd1603ec37d09d01a613f3
treeep <bogdan.grigorenko.jb@gmail.com>
2023-04-29 20:55:50 +0000

    DEV Initial Commit

---
+++ demo/src/main/java/com/cbshop/demo/Cbshop.java
@@ -0,0 +1,13 @@
+package com.cbshop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cbshop {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cbshop.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/cbshop/demo/CbshopTests.java
@@ -0,0 +1,13 @@
+package com.cbshop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbshopTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
