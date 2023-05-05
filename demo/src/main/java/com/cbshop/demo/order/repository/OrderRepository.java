84d24c6f8c23ab8045ef932c36bcf7633eed178f
treeep <bogdan.grigorenko.jb@gmail.com>
2023-05-05 21:15:56 +0000

    feat(dev/#14-checking-and-fixing-bugs-by-order) make method return Optional for better readability

--- demo/src/main/java/com/cbshop/demo/order/repository/OrderRepository.java
+++ demo/src/main/java/com/cbshop/demo/order/repository/OrderRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Page<Order> getOrdersByUserId(@Param("user_id") long user_id, Pageable pageable);
+    Optional<Page<Order>> getOrdersByUserId(@Param("user_id") long user_id, Pageable pageable);
 }
