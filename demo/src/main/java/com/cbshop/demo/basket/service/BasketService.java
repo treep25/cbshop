500c3a9cadf110450b60a4c347fc0965f5c342f8
treeep <bogdan.grigorenko.jb@gmail.com>
2023-05-07 11:00:12 +0000

    fix(dev/#15-check-password-reset-logic-and-scheduler) add constants for String

--- demo/src/main/java/com/cbshop/demo/basket/service/BasketService.java
+++ demo/src/main/java/com/cbshop/demo/basket/service/BasketService.java
@@ -14,25 +14,26 @@ import java.util.List;
 public class BasketService {
 
     private final ProductRepository productRepository;
+    private final static String BASKET = "basket";
 
     public void addProductsToBasket(HttpSession session, List<Long> productIds) {
-        session.setAttribute("basket", productRepository.findAllById(productIds));
+        session.setAttribute(BASKET, productRepository.findAllById(productIds));
     }
     @SuppressWarnings("unchecked")
     public void removeFromBasket(HttpSession session, List<Long> productIds) {
-        List<Product> basket = (List<Product>) session.getAttribute("basket");
+        List<Product> basket = (List<Product>) session.getAttribute(BASKET);
 
         basket.removeIf(product -> productIds.contains(product.getId()));
 
-        session.setAttribute("basket", basket);
+        session.setAttribute(BASKET, basket);
     }
 
     public void clearBasket(HttpSession session){
-        session.removeAttribute("basket");
+        session.removeAttribute(BASKET);
     }
 
     @SuppressWarnings("unchecked")
     public List<Product> getBasket(HttpSession session){
-        return (List<Product>) session.getAttribute("basket");
+        return (List<Product>) session.getAttribute(BASKET);
     }
 }
